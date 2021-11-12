f74ddc0484e50bd34e467927728f9a456006c436
hjalmar jakobsson <hjalmar.jakobsson@telia.com>
2021-11-12 15:57:02 -0500

    save

--- lua/telescope/_extensions/repo.lua
+++ lua/telescope/_extensions/repo.lua
@@ -1,5 +1,7 @@
 local repo_builtin = require'telescope._extensions.repo_builtin'
 
+print("local telescope repo")
+
 return require'telescope'.register_extension{
   exports = {
     list = repo_builtin.list,
